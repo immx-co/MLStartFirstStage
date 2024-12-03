0dc99320eb79b383c4014817ed7057390619fe1d
Вячеслав Орлов <slava.orlov.2001@mail.ru>
2024-12-04 00:51:35 +0300

    added part of the frontend to the 3rd stage

---
+++ ThirdStage/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ThirdStage.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ ThirdStage/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ThirdStage.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
